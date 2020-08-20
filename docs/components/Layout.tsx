983532efa26ad16309b009cbc983e707726f219a
Charlie ⚡ <charlespeters42@gmail.com>
2020-08-20 15:40:19 -0700

    adding more flags and updating docs

--- docs/components/Layout.tsx
+++ docs/components/Layout.tsx
@@ -1,27 +1,40 @@
 import React, { ReactNode } from "react";
 import { FiGithub } from "react-icons/fi";
 import Head from "next/head";
+import classNames from "@sindresorhus/class-names";
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div className="bg-black text-white">
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header className="flex justify-end p-2">
-      <FiGithub size={24}/>
-    </header>
-    {children}
-    <footer className="font-mono text-center px-2 py-4">
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-);
+function getEmojiLink(): string {
+  const emoji = "🏳️‍🌈";
+  return `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`;
+}
+
+const Layout = ({ children, title = "This is the default title" }: Props) => {
+  const link = getEmojiLink();
+
+  const cx = "..." || classNames("bg-black", "text-white");
+
+  return (
+    <div className={cx}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href={link} />
+      </Head>
+      <header className="flex justify-end p-2">
+        <FiGithub size={24} />
+      </header>
+      {children}
+      <footer className="px-2 py-4 font-mono text-center">
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  );
+};
 
 export default Layout;
--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -2,50 +2,76 @@ import Layout from "../components/Layout";
 import { NextPage } from "next";
 import classNames from "@sindresorhus/class-names";
 
-const BASE = "shadow-md rounded w-full h-32 mb-2";
+const BASE = "shadow-md rounded w-full h-24 mb-2";
 
-const Sample: React.FC<{ className: string, label?: string; }> = props => {
+const Sample: React.FC<{ className: string; label?: string }> = props => {
   return (
     <div className="mb-16">
       <div className={classNames(BASE, props.className)} role="img"></div>
-      {props.label ? <h4 className="font-mono text-sm tracking-widest opacity-50 uppercase">{props.label}</h4> : null}
+      {props.label ? (
+        <h4 className="font-mono text-sm tracking-widest opacity-50 uppercase">
+          {props.label}
+        </h4>
+      ) : null}
     </div>
   );
 };
 
+const ContentTitle: React.FC = props => {
+  return (
+    <h2 className="text-3xl mb-8 font-sans font-normal">{props.children}</h2>
+  );
+};
+
+const ContentArea: React.FC<{ className?: string }> = props => {
+  const cx = classNames("px-2 container mx-auto", props.className);
+  return <section className={cx}>{props.children}</section>;
+};
+
+// POC Pride
+
 const IndexPage: NextPage = () => (
   <Layout title="LGBTQ.css">
-    <header className="text-center py-32 px-2">
-      <h1 className="font-mono text-5xl font-bold mb-4">🌈 <br />LGBTQ.css</h1>
-      <p className="text-md font-mono uppercase tracking-widest">adding some pride to your stylesheets</p>
+    <header className="container mx-auto py-32 px-2">
+      <h1 className="text-5xl font-bold mb-4">
+        🌈 <br />
+        LGBTQ.css
+      </h1>
+      <p className="text-md font-mono uppercase tracking-widest opacity-50">
+        adding some pride to your stylesheets
+      </p>
     </header>
-
-    <section className="px-2 container mx-auto">
-      <h2 className="text-3xl mb-4 font-sans font-black">Flags</h2>
-
+    <ContentArea>
+      <ContentTitle>Flags</ContentTitle>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-        <Sample className="Gay" label="Gay Pride"/>
-        <Sample className="BiPlus" label="Bisexual Pride"/>
-        <Sample className="Trans" label="Transgender Pride"/>
-        <Sample className="NonBinary" label="Non-binary Pride"/>
-        <Sample className="Asexual" label="Asexual Pride"/>
+        <Sample className="Gay" label="Gay Pride" />
+        <Sample className="BiPlus" label="Bisexual Pride" />
+        <Sample className="Trans" label="Transgender Pride" />
+        <Sample className="NonBinary" label="Non-binary Pride" />
+        <Sample className="Asexual" label="Asexual Pride" />
         <Sample className="Pansexual" label="Pansexual Pride" />
-        <Sample className="Queer" label="Queer Pride"/>
-        <Sample className="GayMale" label="Gay Male Pride"/>
-        <Sample className="Lesbian" label="Lesbian Pride"/>
-        <Sample className="Intersex" label="Intersex Pride"/>
-        <Sample className="GenderFluid" label="Genderfluid Pride"/>
-        <Sample className="Agender" label="Agender Pride"/>
+        <Sample className="Queer" label="Queer Pride" />
+        <Sample className="GayMale" label="Gay Male Pride" />
+        <Sample className="Lesbian" label="Lesbian Pride" />
+        <Sample className="Intersex" label="Intersex Pride" />
+        <Sample className="GenderFluid" label="Genderfluid Pride" />
+        <Sample className="Agender" label="Agender Pride" />
       </div>
-    </section>
-    <section className="px-2 container mx-auto mb-32">
-      <h2 className="text-3xl mb-4 font-sans font-black">About</h2>
-      <p> FlagsSome simple CSS gradients to add some pride to your stylesheets. Color values are slightly saturated for preference.</p>
-      
-    </section>    <section className="px-2 container mx-auto mb-64">
-      <h2 className="text-3xl mb-4 font-sans font-black">Usage</h2>
+    </ContentArea>
+    <ContentArea className="mb-32">
+      <article className="max-w-lg">
+        <ContentTitle>About</ContentTitle>
+
+        <p>
+          FlagsSome simple CSS gradients to add some pride to your stylesheets.
+          Color values are slightly saturated for preference.
+        </p>
+      </article>
+    </ContentArea>
+    <ContentArea className="mb-64">
+      <ContentTitle>Usage</ContentTitle>
       <div className="md:grid grid-cols-6 gap-4"></div>
-    </section>
+    </ContentArea>
   </Layout>
 );
 
